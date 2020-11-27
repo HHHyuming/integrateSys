87e323dbe8c00a2710e3cb61a190aa17b8dd7fe6
huangym <2219659698@qq.com>
2020-11-27 17:59:09 +0800

    work done

--- backend/itegrateSys/itegrateSys/urls.py
+++ backend/itegrateSys/itegrateSys/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('user/', include('user_center.urls'))
+    path('user/', include('user_center.urls')),
+    path('system_manage/', include('system_manager.urls')),
 ]
